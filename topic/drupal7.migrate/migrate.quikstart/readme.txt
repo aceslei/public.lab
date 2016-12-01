a89d4a505b7c30cf905d3de46aa4e8cbfd2ea742
dreftymac <dreftymac@gmail.com>
2016-12-01 12:23:55 -0800

    general_update ;; 20161201:113654 ;;
    	modified:   topic/drupal7.migrate/migrate.quikstart/readme.txt
    
    	topic/python/

--- topic/drupal7.migrate/migrate.quikstart/readme.txt
+++ topic/drupal7.migrate/migrate.quikstart/readme.txt
@@ -13,7 +13,32 @@
 ###         * __seealso__
 ### <end-file_info>
 
+head:
+  
+  - caption:      processing steps
+    dreftymacid: bruiser_recur_vasty
+    rectype:  rrmini05
+    datebeg:  Wed Nov 30 12:53:58 2016 
+    desc:     |
+      ## overview of the steps for running drupal d7 migrate
+      
+      * create target_content_types on target_drupalsite
+          * manually
+          * bundle_copy
+          * features
+      * prepare the data connect environment
+          * mysql source to target_drupalsite via settings.php
+      * create the module files
+      
+          
+    seealso:  |
+      * __seealso__
+    wwbody: |
+      
+
+
 main:
+  
   - caption:  created content type from the drupalize.me tutorial
     dreftymacid: regard_drearier_revising
     rectype:  rrmini05
---
+++ topic/python/string.format/demo.subclass.string.001.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+'''
+### <beg-file_info>
+### main:
+###   - date: created="Thu Dec 01 12:03:13 2016"
+###     last: lastmod="Thu Dec 01 12:03:13 2016"
+###     tags: python,string,format,subclass,oop,py2
+###     author: created="dreftymac"
+###     dmid: "narceine_land_brains"
+###     filetype: "py"
+###     lastupdate: "__lastupdate__"
+###     desc: |
+###         * demo code showing an alternate use of string.format()
+###     seealso: |
+###         * __seealso__
+### <end-file_info>
+'''
+
+### ----------------------------------
+if('init_python'):
+  import re
+  import yaml
+  import textwrap
+  from datetime import datetime
+
+### ----------------------------------
+if('init_custom_formatter'):
+  class CustFmt(str):
+    '''extend python str type to permit custom delimiters on str.format()
+    '''
+    def __new__(cls, value, srcdata={}, tkbeg='<%',tkend='%>',):
+      return str.__new__(cls,value)
+    def __init__(self,value,srcdata={}, tkbeg='<%',tkend='%>'):
+      self.srcdata = srcdata;self.tkbeg = tkbeg;self.tkend = tkend;
+    def myformat(self): return(self.__str__()
+      .replace('{','{{').replace('}','}}')
+      .replace(self.tkbeg,'{').replace(self.tkend,'}')
+      .format(**self.srcdata))
+
+### ----------------------------------
+if('show_demo_usage::curly-brace-collisions-fixed'):
+
+  odata   =   dict({"template":'string'})
+  vout    =   CustFmt('''
+  This is a <%template%> this is a {template}
+  ''',odata).myformat()
+  print vout
+
+### ----------------------------------
+if('show_demo_usage::fill-in-the-blank-document-generation'):
+
+
+  ### ------------------------------------------------------------------------
+  odata   =   yaml.safe_load('''
+    maininfo:
+      username:   tvt173
+      userid:     701803
+      greeter:    dreftymac
+  ''')
+
+  ### ------------------------------------------------------------------------
+  vout    =   CustFmt('''
+  ## Overview
+
+  This is an introductory document written to  from .
+
+  ### Introduction
+
+
+  Greetings!  I noticed you are a user on Stackoverflow!
+
+  Your user page is located here:
+
+  Thanks for using the site! We programmers need to stick together! ^_^
+
+  ''',odata).myformat()
+  print vout

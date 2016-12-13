750adeea63ae1782bcc345d23bbde409aae62ab9
dreftymac <dreftymac@gmail.com>
2016-12-12 19:24:24 -0800

    here we do not care about empty brace autonumbering
    	modified:   topic/python/string.format/demo.subclass.string.002.py

--- topic/python/string.format/demo.subclass.string.002.py
+++ topic/python/string.format/demo.subclass.string.002.py
@@ -20,51 +20,57 @@
 ### ----------------------------------
 if('init_python'):
   import re
+  import string
   import textwrap
   import yaml
-  import string
-  from math import floor, log10
 
 ### ----------------------------------
 if('init_custom_formatter'):
-  class CustFormatter(string.Formatter): ## YES_WORKY with autonumbering
-      "Defines special formatting"
+  class CuFo001(string.Formatter): ## YES_WORKY with autonumbering
+      '''
+      CuFo001 -- testing override of string.format()
+      * here we do not care about empty brace {} autonumbering
+      '''
       def __init__(self):
-          super(CustFormatter, self).__init__()
-          self.last_number = 0
-
-      def get_value(self, key, args, kwargs):
-          if key == '':
-              key = self.last_number
-              self.last_number += 1
-          return super(CustFormatter, self).get_value(key, args, kwargs)
-
-      def powerise10(self, x):
-          if x == 0: return 0, 0
-          Neg = x < 0
-          if Neg: x = -x
-          a = 1.0 * x / 10**(floor(log10(x)))
-          b = int(floor(log10(x)))
-          if Neg: a = -a
-          return a, b
-
-      def eng(self, x):
-          a, b = self.powerise10(x)
-          if -3 < b < 3: return "%.4g" % x
-          a = a * 10**(b%3)
-          b = b - b%3
-          return "%.4g*10^%s" % (a, b)
+        super(CuFo001, self).__init__()
 
       def format_field(self, value, format_string):
         # handle an invalid format
         if format_string == "i":
             return self.eng(value)
         else:
-            return super(CustFormatter,self).format_field(value, format_string)
+            return super(CuFo001,self).format_field(value, format_string)
 
 ### ----------------------------------
 if('test_custom_formatter'):
-  fmt = CustFormatter()
+
+  odata   =   yaml.safe_load('''
+    project: Testing123
+    django_info:
+      engine:     django.db.backends.postgresql
+      dbname:     mytestdb
+      dbuser:     mydbuser
+      dbpass:     my/weak/password
+      dbhost:     127.0.0.1
+      dbport:     5432
+    user_table:
+      - username: alphadog
+        userrowid: 1
+        useremail: alpha@gg.com
+      - username: bravomopp
+        userrowid: 2
+        useremail: bravo@gg.com
+      - username: charlblobs
+        userrowid: 3
+        useremail: charlie@complicatedmail.com
+    hosts_table:
+      - hostname:  .coffeehouse.com
+        hostaddr:  12.12.12.12
+      - hostname:  .teahouse.com
+        hostaddr:  14.14.14.14
+  ''')
+
+  fmt = CuFo001()
   print('{}'.format(0.055412))
   print(fmt.format("{0:i} ", 55654654231654))
   print(fmt.format("{}", 0.00254641))

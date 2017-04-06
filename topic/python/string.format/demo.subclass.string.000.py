f1bc8ea237444dc2f77259b6f09f524ed0636e6e
smosley <smosley@uoregon.edu>
2017-04-05 20:46:32 -0700

    .render() method not required because of def __str__ self
    	modified:   topic/python/string.format/demo.subclass.string.000.py

--- topic/python/string.format/demo.subclass.string.000.py
+++ topic/python/string.format/demo.subclass.string.000.py
@@ -18,15 +18,7 @@
 """
 
 if('py_init_class'):
-  class PowerString(str):
-    #def __new__(cls, strval):
-      #ob = super(PowerString, cls).__new__(cls, string)
-      #vout = self
-      #return vout
-      #self.strval = str(strval)
-      #return str.__new__(cls, value)
-      #return
-
+  class PowerString(object):
     def __init__(self, strval=None, ddata=None ):
       if strval is None:
         self.strval = ""
@@ -37,6 +29,12 @@ if('py_init_class'):
         self.ddata = {}
       elif (True):
         self.ddata = ddata
+    ##;;
+
+    def aanoop(self,*args):
+      print args
+      self.strval = self.strval
+      return self
     ##;;
 
     def title(self):
@@ -50,31 +48,77 @@ if('py_init_class'):
     ##;;
 
     def render(self):
-      vout = super(PowerString, self).title()
       return str(self.strval)
     ##;;
 
-    def __getattr__(self, name):
-      def _missing(*args, **kwargs):
-        print "A missing method was called."
-        print "The object was %r, the method was %r. " % (self, name)
-        print "It was called with %r and %r as arguments" % (args, kwargs)
-      return _missing
-      #return self
+    def __str__(self):
+      return self.render()
     ##;;
 
-if(not not 'py_demo_process'):
-  mytest =  PowerString("hello world").reverse().title().render()
-  print mytest
+    def __getattr__(self, name):
+      ##
+      try:
+        self.strval = getattr(str,name)(self.strval)
+      except Exception: pass
+      ##
+      return self.aanoop
+    ##;;
+
+    # def __getattr__(self, name):
+    #   if(True):
+    #     self.strval = getattr(str,name)(self.strval)
+    #     #return self.__new__(self.strval,self.ddata)
+    #   elif(True):
+    #     def _missing(*args, **kwargs):
+    #       print "A missing method was called."
+    #       print "The object was %r, the method was %r. " % (self, name)
+    #       print "It was called with %r and %r as arguments" % (args, kwargs)
+    #     return _missing
+    #   #return self
+    # ##;;
+
+if('demo_holdingsqpalan'):
+  ## all of these work as expected and desired
+  print PowerString("hello world").render()
+  print PowerString("hello world").reverse().render()
+  print PowerString("hello world").title().reverse().render()
+  print PowerString("hello world").reverse().title().render()
+  print PowerString("hello world").startswith('hello')
   pass
 
-if(not 'py_demo_process'):
-  mytest =  PowerString("hello world").render()
-  print mytest
+if(not 'demo_holdingsqlalan'):
+  ## all of these work as expected and desired
+  print PowerString("hello world").upper().reverse().render()
   pass
 
-if(not 'py_demo_process'):
-  PowerString("hello world").noexisto(1,2,*[3,4,5],**{'1':'1'})
+if( 'demo_holdingsqmalan'):
+  ## all of these work as expected and desired
+  pass
+
+if( 'demo_holdingsqnalan'):
+  ## all of these work as expected and desired
+  pass
 
 
+# if('demo002'):
+#   print PowerString("hello world").render()
+#   print PowerString("hello world").reverse().render()
+#   print PowerString("hello world").title().reverse().render()
+#   print PowerString("hello world").reverse().title().render()
+#   pass
 
+# if(not not 'py_demo_process'):
+#   mytest =  PowerString("hello world").reverse().title().render()
+#   print mytest
+#   pass
+#
+# if(not not 'py_demo_process'):
+#   print type(PowerString("hello world").upper)
+#
+# if(not 'py_demo_process'):
+#   mytest =  PowerString("hello world").reverse().title().render()
+#   print mytest
+#   pass
+
+# if(not 'py_demo_process'):
+#   print PowerString("hello world").noexisto(1,2,*[3,4,5],**{'1':'1'})
